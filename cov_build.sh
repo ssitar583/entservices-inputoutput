3284179d1522fd015a698702787a2d9b7dc4243c
agampa263 <133222558+agampa263@users.noreply.github.com>
2025-05-20 17:37:22 +0530

    RDKEMW-4139: Coverity integration with Entservices-inputoutput repo
    Reason for change: Coverity integration with middleware component workflow
    Test Procedure: Verify checks in github
    Risks: Low
    Signed-off-by:AkshayKumar_Gampa AkshayKumar_Gampa@comcast.com

--- cov_build.sh
+++ cov_build.sh
@@ -49,7 +49,8 @@ cmake -G Ninja -S "$GITHUB_WORKSPACE" -B build/entservices-inputoutput \
 -include ${GITHUB_WORKSPACE}/entservices-testframework/Tests/mocks/wpa_ctrl_mock.h \
 -include ${GITHUB_WORKSPACE}/entservices-testframework/Tests/mocks/secure_wrappermock.h \
 -include ${GITHUB_WORKSPACE}/entservices-testframework/Tests/mocks/HdmiCec.h \
---coverage -Wall -Werror -Wno-error=format \
+--coverage \
+-Wall -Wno-unused-result -Werror -Wno-error=format \
 -Wl,-wrap,system -Wl,-wrap,popen -Wl,-wrap,syslog \
 -DENABLE_TELEMETRY_LOGGING -DUSE_IARMBUS \
 -DENABLE_SYSTEM_GET_STORE_DEMO_LINK -DENABLE_DEEP_SLEEP \
