8024b0556bb82ea12c6a864a4a3f782c7eeb40d9
kprathyusha <kprathyusha@synamedia.com>
2025-06-02 18:07:57 +0530

    error in test_foo

---
+++ Tests/L2Tests/tests/test_foo.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+#include <iostream>
+
+
+
+#error 777
+class PrintTestIO : public ::testing::Test {
+};
+
+// Single test with print statement
+TEST_F(PrintTestIO, BasicOutputIO) {
+    std::cout << "this is a print statement from inputoutput" << std::endl;
+}
+
+TEST_F(PrintTestIO, BasicoutputIOExt) {
+    int service =10;
+    ASSERT (service != 10);
+
+    std::string name = "Test";
+
+   // EXPECT_EQ( 0, name == "Test");
+   // values=10;
+    std::cout << "this is a second print statement from inputoutput" << std::endl;
+}
+
+
+
